de5c21f0b2169e64670d077d703a6ab8eea6100f
zahir535 <zahirco535@gmail.com>
2024-06-22 20:31:26 +0800

    firebase collection names

---
+++ dist/data/collectionNames.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.COLLECTION_NAMES = void 0;
+exports.COLLECTION_NAMES = {
+    PI_VALUES: "pi-values",
+};
+//# sourceMappingURL=collectionNames.js.map
